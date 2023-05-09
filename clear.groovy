e083357d20e371d809c120e9c4fe6fa31c927951
deadcomedian <ilya-sharov-2000@mail.ru>
2023-05-09 17:14:58 +0300

    modify

--- clear.groovy
+++ clear.groovy
@@ -1,3 +1,14 @@
+properties([
+  parameters([
+    string(name: 'modelName', description: 'Model name')
+  ]),
+  pipelineTriggers([
+    parameterizedCron('''
+        */2 * * * * %modelName=churn_model_
+    ''')
+  ])
+])
+
 node {
     stage ("Clear cluster") {
         withCredentials([file(credentialsId: 'KUBECONFIG', variable: 'KUBECONFIG')]) {
